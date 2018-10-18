a09b33bbd7afe26d31742cc4ea4c090263c26a77
Sam Bossley <31111702+samaraikun@users.noreply.github.com>
2018-10-18 01:28:41 -0400

    fixed script/style minifier

---
+++ test/assets/test.js
@@ -0,0 +1,11 @@
+
+// does this work??
+
+document.documentElement.style.backgroundColor = 'lightblue';
+
+/* will this property  still be applied     after
+the minify         process occurs?
+*/
+
+// this is an example of bad code:
+console.log('testing // if this works through /* trial and error */');
